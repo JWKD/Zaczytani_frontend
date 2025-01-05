ce4805613c15b0ad09abe6ba370e87c5888f0a13
SylwekD <rutki2268@gmail.com>
2025-01-05 18:23:14 +0100

    PT2024ZACZ-96 add fixes

--- src/pages/ReportUserPage.tsx
+++ src/pages/ReportUserPage.tsx
@@ -3,22 +3,45 @@ import ReportUser from '../components/ReportUser/ReportUser';
 import { ReviewPage } from '../interfaces/review';
 import { useEffect, useState } from 'react';
 import reviewApi from '../api/reviewApi';
+import CatLoader from '../components/CatLoader/CatLoader';
 
 function ReportUserPage() {
   const { id } = useParams<{ id: string }>();
   const [review, setReview] = useState<ReviewPage>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      if (id) {
-        const result = await reviewApi.getReview(id);
-        setReview(result);
+      try {
+        if (id) {
+          const result = await reviewApi.getReview(id);
+          setReview(result);
+        }
+      } catch (err) {
+        setError('Wystąpił błąd');
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, [id]);
-  if (review) return <ReportUser {...review} />;
+  if (review)
+    return loading ? (
+      <div
+        style={{
+          width: '500px',
+          margin: '0 auto',
+        }}
+      >
+        <CatLoader />
+      </div>
+    ) : error ? (
+      <div>Error: {error}</div>
+    ) : (
+      <ReportUser {...review} />
+    );
 }
 
 export default ReportUserPage;
