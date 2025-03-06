1bb650549278c43320034121c469767ac257f4c9
Sylwester312 <sylwester.kowalewski12@gmail.com>
2025-03-06 22:02:00 +0100

    [PT2024ZACZ-85] - Shelf download change from the server.

--- src/components/ShelfComponent/ShelfComponent.tsx
+++ src/components/ShelfComponent/ShelfComponent.tsx
@@ -1,55 +1,20 @@
-import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import defaultImage from '../../assets/defaultCover.jpg';
 import defaultImageBlank from '../../assets/DefaultBookCover.png';
 import styles from './ShelfComponent.module.scss';
-import { Book } from '../../interfaces/book';
 import { Shelf } from '../../interfaces/Shelf';
-import shelfApi from '../../api/shelvesApi';
-import CatLoader from '../CatLoader/CatLoader';
-
 export interface ShelfComponentProps {
   shelf: Shelf;
 }
 
 const ShelfComponent: React.FC<ShelfComponentProps> = ({ shelf }) => {
-  const [books, setBook] = useState<Book[]>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await shelfApi.getShelfBooks(shelf.id);
-        setBook(result);
-      } catch (err) {
-        setError('Wystąpił błąd');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [shelf.id]);
-
-  return loading ? (
-    <div
-      style={{
-        width: '100px',
-        margin: '0 auto',
-      }}
-    >
-      <CatLoader />
-    </div>
-  ) : error ? (
-    <div>Error: {error}</div>
-  ) : (
+  return (
     <div className={styles.shelfContainer}>
       <div className={styles.booksContainer}>
-        {books
+        {shelf.imageUrl
           ?.slice(0, 2)
-          .map((books: Book, index) => (
-            <img key={index} src={books.imageUrl || defaultImage} className={styles.images} alt={books.title} />
+          .map((image: string, index) => (
+            <img key={index} src={image || defaultImage} className={styles.images} alt={`okładka`} />
           ))}
         <Link to={`/bookshelf/getBookshelf/${shelf.id}`} className={styles.imageWithText}>
           <img src={defaultImageBlank} className={styles.images} alt="Book Cover" />
